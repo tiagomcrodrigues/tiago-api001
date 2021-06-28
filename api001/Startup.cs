e724508eadc62c1532cad53df3761684993f6fcb
Tiago Rodrigues <tiagomcoutorodrigues@gmail.com>
2021-06-28 14:24:39 -0300

    finalizado a api02

--- api001/Startup.cs
+++ api001/Startup.cs
@@ -77,12 +77,13 @@ namespace api001
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api001 v1"));
-            }
+            //if (env.IsDevelopment())
+            // {
+            //    app.UseDeveloperExceptionPage();
+            //    app.UseSwagger();
+            //   app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api001 v1"));
+            // }
+            app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
 
